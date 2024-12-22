d3a2415f5127a83e7f2828d2f69e9fd3e4a6fae1
Mahajan-Sachin <152908416+Mahajan-Sachin@users.noreply.github.com>
2024-12-23 01:25:08 +0530

    Time: 0 ms (100%), Space: 12.2 MB (41.1%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int k = m + n;  // The total size of the merged array
-        vector<int> arr(k);  // Create a new array to store the merged result
+        int i = m - 1;  // Pointer for nums1 (last valid element)
+        int j = n - 1;  // Pointer for nums2 (last element)
+        int k = m + n - 1;  // Pointer for the last position in nums1
 
-        // Copy elements from nums1 to arr
-        for (int i = 0; i < m; i++) {
-            arr[i] = nums1[i];
+        // Merge the arrays in reverse order
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];  // Place the larger element at the end
+            } else {
+                nums1[k--] = nums2[j--];  // Place the larger element at the end
+            }
         }
 
-        // Copy elements from nums2 to arr, starting from where nums1 ends
-        for (int j = 0; j < n; j++) {
-            arr[m + j] = nums2[j];
+        // If there are any elements left in nums2, copy them
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
         }
 
-        // Now sort the new array
-        sort(arr.begin(), arr.end());
-
-        // Copy the merged and sorted array back to nums1
-        for (int i = 0; i < k; i++) {
-            nums1[i] = arr[i];
-        }
+        // No need to handle nums1 because they are already in place
     }
 };
