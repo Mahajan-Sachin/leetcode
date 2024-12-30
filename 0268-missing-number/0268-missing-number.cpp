a8f3b40deea944038b1813dac11068c896fd0dd0
Mahajan-Sachin <152908416+Mahajan-Sachin@users.noreply.github.com>
2024-12-31 00:01:27 +0530

    Time: 0 ms (100%), Space: 21.8 MB (43.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,9 +3,7 @@ public:
     int missingNumber(vector<int>& nums) {
         int sum_arr=0;
         int n=nums.size();
-        for(int i=0;i<nums.size();i++){
-            sum_arr=nums[i]+sum_arr;
-        }
+        for(int i=0;i<nums.size();i++) sum_arr=nums[i]+sum_arr;
         int actual=n*(n+1)/2;
         return actual-sum_arr;
     }
