f4d3a8dfde785e22f62ea64867f1a960bd601597
Sachin Mahajan <152908416+Mahajan-Sachin@users.noreply.github.com>
2025-06-16 14:40:41 +0530

    Time: 76 ms (92.06%), Space: 28 MB (30.55%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -11,12 +11,6 @@ class Solution:
         ptrA=headA
         ptrB=headB
         while ptrA!=ptrB:
-            if ptrA is None:
-                ptrA=headB
-            else:
-                ptrA=ptrA.next
-            if ptrB is None:
-                ptrB=headA
-            else:
-                ptrB=ptrB.next
+            ptrA=ptrA.next if ptrA else headB
+            ptrB=ptrB.next if ptrB else headA
         return ptrA
