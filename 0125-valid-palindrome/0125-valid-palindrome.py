4b8d254a98b89557960beeacb53c37a05b2f4d39
Sachin Mahajan <152908416+Mahajan-Sachin@users.noreply.github.com>
2025-06-27 09:41:15 +0530

    Time: 11 ms (36.13%), Space: 18 MB (83.25%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -7,9 +7,8 @@ class Solution:
         left=0
         right=len(cleaned)-1
         while(left<right):
-            if cleaned[left]!=cleaned[right]:
-                return False
+            if cleaned[left]!=cleaned[right]: return False
             left+=1
             right-=1
         return True
-        
+
