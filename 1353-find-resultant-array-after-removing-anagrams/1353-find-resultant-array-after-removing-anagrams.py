fde357050ec3e5acbf0dcb78a8db35e87f684c4f
Sachin Mahajan <152908416+Mahajan-Sachin@users.noreply.github.com>
2025-06-30 11:09:16 +0530

    Time: 27 ms (11.39%), Space: 17.7 MB (70.76%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,16 @@
+from collections import Counter
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        result=[words[0]]
+        for i in range(1,len(words)):
+            if Counter(words[i])!=Counter(result[-1]):
+                result.append(words[i])
+        return result
+
+
+        
+             
+
+        
+
+        
