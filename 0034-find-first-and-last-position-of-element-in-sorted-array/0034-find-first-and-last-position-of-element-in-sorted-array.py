f1d7a0b26ee793dab4c292e724d2bf81e1c59308
Sachin Mahajan <152908416+Mahajan-Sachin@users.noreply.github.com>
2025-07-15 10:40:04 +0530

    Time: 3 ms (7.55%), Space: 19.2 MB (20.66%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -4,32 +4,32 @@ class Solution:
         def left(nums,target,result):
             start=0
             end=len(nums)-1
-            left=-1
+            val_left=-1
             while start<=end:
                 mid=int(start+(end-start)/2)
                 if nums[mid]==target:
-                    left=mid
+                    val_left=mid
                     end=mid-1
                 elif nums[mid]<target:
                     start=mid+1
                 else:
                     end=mid-1
-            result.append(left)
+            result.append(val_left)
 
         def right(nums,target,result):
             start=0
             end=len(nums)-1
-            right=-1
+            val_right=-1
             while start<=end:
                 mid=int(start+(end-start)/2)
                 if nums[mid]==target:
-                    right=mid
+                    val_right=mid
                     start=mid+1
                 elif nums[mid]<target:
                     start=mid+1
                 else:
                     end=mid-1
-            result.append(right)
+            result.append(val_right)
         left(nums,target,result)
         right(nums,target,result)
         return result
