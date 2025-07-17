45af6f9ddbb72bf3eb9828d3744b1b2bb4ba510a
Sachin Mahajan <152908416+Mahajan-Sachin@users.noreply.github.com>
2025-07-17 14:56:12 +0530

    Time: 32 ms (70.45%), Space: 17.8 MB (84.13%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
@@ -3,7 +3,7 @@ class Solution:
         for i in range(len(tiles)):
             if visited[i]:
                 continue
-            if i > 0 and tiles[i] == tiles[i - 1] and not visited[i - 1]:
+            if i > 0 and tiles[i] == tiles[i - 1] and visited[i - 1]==False:
                 continue
             visited[i] = True
             count[0] += 1  # Count this new sequence
