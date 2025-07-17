be8e2ac0a0bd0aec3fb2e417d8b3eab3140bf2bb
Sachin Mahajan <152908416+Mahajan-Sachin@users.noreply.github.com>
2025-07-17 14:53:49 +0530

    Time: 27 ms (77.95%), Space: 17.7 MB (95.78%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
@@ -0,0 +1,18 @@
+class Solution:
+    def helper(self, tiles, curr, visited, count):
+        for i in range(len(tiles)):
+            if visited[i]:
+                continue
+            if i > 0 and tiles[i] == tiles[i - 1] and not visited[i - 1]:
+                continue
+            visited[i] = True
+            count[0] += 1  # Count this new sequence
+            self.helper(tiles, curr + tiles[i], visited, count)
+            visited[i] = False  # Backtrack
+
+    def numTilePossibilities(self, tiles: str) -> int:
+        tiles = sorted(tiles)
+        count = [0]
+        visited = [False] * len(tiles)
+        self.helper(tiles, "", visited, count)
+        return count[0]
