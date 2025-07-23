0b8e62f1ed4e60a3ae58f3bdba75515d413143ec
Sachin Mahajan <152908416+Mahajan-Sachin@users.noreply.github.com>
2025-07-23 15:05:15 +0530

    Time: 32 ms (54.04%), Space: 20.3 MB (71.48%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,15 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        track = {0: 1}  # prefix_sum count dictionary
-        curr = 0
-        result = 0
-
-        for num in nums:
-            curr += num
-
-            if (curr - k) in track:
-                result += track[curr - k]  # ✅ add number of times (curr - k) has appeared
-
-            track[curr] = track.get(curr, 0) + 1  # ✅ always update count of current prefix sum
-
+        track={}
+        track[0]=1
+        cum_sum=0
+        result=0
+        for i in range(len(nums)):
+            cum_sum+=nums[i]
+            if cum_sum-k in track:
+                result+=track[cum_sum-k]
+            track[cum_sum]=track.get(cum_sum,0)+1
         return result
+        
