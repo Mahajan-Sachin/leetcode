8c6a00e8a984ecba6fec50efce30457f7ba8c8db
Sachin Mahajan <152908416+Mahajan-Sachin@users.noreply.github.com>
2025-07-25 15:38:11 +0530

    Time: 3 ms (88.61%), Space: 19.1 MB (46.2%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,30 +1,30 @@
 class Solution:
-    def dfs(self,adj,node,visited):
-        visited[node]=True
-        for j in adj[node]:
-            if not visited[j]:
-                self.dfs(adj,j,visited)
-
+    def List_conversion(self,matrix):
+        adj_list=[[] for _ in range(len(matrix))]
+        for u in range(len(matrix)):
+            for v in range(len(matrix)):
+                if u!=v and matrix[u][v]==1: #ya u!=v islia kiya because we dont want to do redduncy because conneted means we want connections to another nodes, not itself
+                    adj_list[u].append(v)
+                    adj_list[v].append(u)
+        return adj_list
+    def bfs(self,start,visited,adj_list):
+        q=deque()
+        q.append(start)
+        visited[start]=True
+        while q:
+            node=q.popleft()
+            for neighbour in adj_list[node]:
+                if not visited[neighbour]:
+                    q.append(neighbour)
+                    visited[neighbour]=True
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        from collections import defaultdict
-        adj=defaultdict(list)
-        rows=colns=len(isConnected)
-        for i in range(rows):
-            for j in range(colns):
-                if isConnected[i][j]==1 and i!=j:
-                    adj[i].append(j)
-                    adj[j].append(i)
-        visited=[False]*rows
+        adj_list=self.List_conversion(isConnected)
+        visited=[False]*len(adj_list)
         count=0
-        for i in range(rows):
+        for i in range(len(adj_list)):
             if not visited[i]:
-                self.dfs(adj,i,visited)
-                count+=1 
+                self.bfs(i,visited,adj_list)
+                count+=1
         return count
-
-
-        
-
-
         
         
