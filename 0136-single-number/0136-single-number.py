71f5bbab9ea1b6b1c9d016e7a9ed88a24d4c0683
Sachin Mahajan <152908416+Mahajan-Sachin@users.noreply.github.com>
2025-07-29 20:06:38 +0530

    Time: 6 ms (35.48%), Space: 20 MB (8.67%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        from collections import Counter
+        freq=Counter(nums)
+        for num in freq:
+            if freq[num]==1:
+                return num
+        
