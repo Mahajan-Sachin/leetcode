99aa62bffafa6f689adf30c95003d3c78bc4a44a
Sachin Mahajan <152908416+Mahajan-Sachin@users.noreply.github.com>
2025-08-02 21:03:24 +0530

    Time: 0 ms (100%), Space: 21.3 MB (74.59%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -5,35 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minvalue(self,node):
-        current=node
-        while(current and current.left):
-            current=current.left
-        return current
+    def mini(self,node):
+        while node.left:
+            node=node.left
+        return node
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
             return None
-        #left exist
-        if key < root.val:
-            root.left = self.deleteNode(root.left, key)
-
-        elif key > root.val:
-            root.right = self.deleteNode(root.right, key)
-        else:
-            #1 child exist, let say left
-            if root.left and not root.right:
-                return root.left
-            #if right exists not left
-            if root.right and not root.left:
+        if key<root.val:
+            root.left=self.deleteNode(root.left,key)
+        elif key>root.val:
+            root.right=self.deleteNode(root.right,key)
+        elif key==root.val:
+            if not root.left and root.right:
                 return root.right
-            # if both dont exist means root is leaf
-            if not root.left and not root.right:
+            elif not root.right and root.left:
+                return root.left
+            elif not root.left and not root.right:
                 return None
-            #both exist
-            else:
-                success=self.minvalue(root.right)
-                root.val=success.val
-                root.right=self.deleteNode(root.right,root.val)
+            elif root.left and root.right:
+                succesor=self.mini(root.right)
+                root.val=succesor.val
+                root.right=self.deleteNode(root.right,succesor.val)
         return root
-
-            
+        
